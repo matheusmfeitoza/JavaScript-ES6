2a74213e7ebe9c11c7026dc865be2fc36760e225
Matheus Marinho <feitozamatheus123@gmail.com>
2021-08-29 17:36:47 -0300

    Adding more about arrays in readme

--- Base/script.js
+++ Base/script.js
@@ -1,28 +1,50 @@
-// Retorne um número aleatório
-// entre 1050 e 2000
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const primeiroValor = comidas.shift();
+console.log(`Primeiro valor do array: ${primeiroValor}`);
+// Remova o último valor de comidas e coloque em uma variável
+const ultimoValor = comidas.pop();
+console.log(`Último valor do array: ${ultimoValor}`)
+// Adicione 'Arroz' ao final da array
+comidas.push('Arroz')
+console.log(comidas)
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift("Peixe","Batata")
+console.log(comidas)
 
-const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050)
-console.log(aleatorio)
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+estudantes.sort();
+console.log(estudantes)
+// Inverta a ordem dos estudantes
+estudantes.reverse();
+console.log(estudantes)
+// Verifique se Joana faz parte dos estudantes
+console.log(`Joana faz parte de estudantes? ${estudantes.includes('Joana') ? "Sim": "Não"}`)
+// Verifique se Juliana faz parte dos estudantes
+console.log(`Juliana faz parte de estudantes? ${estudantes.includes('Juliana')?'sim':'Não'}`)
 
-
-// Retorne o maior número da lista abaixo
-const numeros = '4, 5, 20, 8, 9';
-const newNumeros = numeros.split(', ');
-console.log(Math.max(...newNumeros))
-
-
-// Crie uma função para limpar os preços
-// e retornar os números com centavos arredondados
-// depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-                     'R$ 230  ', 'r$  200'];
-
-function limpaPreco(){
-    let total = 0;
-    listaPrecos.forEach((item)=>{
-        let newPreco = +item.slice(3).trim().replace(',','.');
-        return total += newPreco
-    })
-    console.log(total)
-}
-limpaPreco()
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+let removeSection = html.split('section');
+console.log(removeSection)
+let insereUL = removeSection.join('ul')
+console.log(insereUL)
+let removeDiv = insereUL.split('div')
+console.log(removeDiv)
+let insereLi = removeDiv.join('li')
+console.log(insereLi)
+//
+//
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const arrayOriginal = carros.slice(0);
+carros.pop();
+console.log(arrayOriginal);
+console.log(carros);
--- readme.md
+++ readme.md
@@ -1016,3 +1016,57 @@ limpaPreco()
 
 ### Array
 
+
+Exercício 21:
+```js
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const primeiroValor = comidas.shift();
+console.log(`Primeiro valor do array: ${primeiroValor}`);
+// Remova o último valor de comidas e coloque em uma variável
+const ultimoValor = comidas.pop();
+console.log(`Último valor do array: ${ultimoValor}`)
+// Adicione 'Arroz' ao final da array
+comidas.push('Arroz')
+console.log(comidas)
+// Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift("Peixe","Batata")
+console.log(comidas)
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+estudantes.sort();
+console.log(estudantes)
+// Inverta a ordem dos estudantes
+estudantes.reverse();
+console.log(estudantes)
+// Verifique se Joana faz parte dos estudantes
+console.log(`Joana faz parte de estudantes? ${estudantes.includes('Joana') ? "Sim": "Não"}`)
+// Verifique se Juliana faz parte dos estudantes
+console.log(`Juliana faz parte de estudantes? ${estudantes.includes('Juliana')?'sim':'Não'}`)
+
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
+let removeSection = html.split('section');
+console.log(removeSection)
+let insereUL = removeSection.join('ul')
+console.log(insereUL)
+let removeDiv = insereUL.split('div')
+console.log(removeDiv)
+let insereLi = removeDiv.join('li')
+console.log(insereLi)
+//
+//
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const arrayOriginal = carros.slice(0);
+carros.pop();
+console.log(arrayOriginal);
+console.log(carros);
+```
