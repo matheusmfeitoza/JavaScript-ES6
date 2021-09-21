9feb6c1c6dd6db2e4af026fb7e886da165c62074
Matheus Marinho <feitozamatheus123@gmail.com>
2021-09-21 14:46:44 -0300

    Refactor scroll Suave

--- scripts/modulos/scroll-suave.js
+++ scripts/modulos/scroll-suave.js
@@ -1,17 +1,34 @@
-export default function initialScrollSuave() {
-  const getInternalLinks = document.querySelectorAll('[data-menu] a[href^="#"');
-  function scrollSuaveSections(event) {
+export default class ScrollSuave {
+  constructor(link, option) {
+    this.links = document.querySelectorAll(link);
+    if (option === undefined) {
+      this.option = {
+        behavior: "smooth",
+        block: "start",
+      };
+    } else {
+      this.option = option;
+    }
+    this.scrollSuaveSections = this.scrollSuaveSections.bind(this);
+  }
+
+  scrollSuaveSections(event) {
     event.preventDefault();
     const getHref = event.currentTarget.getAttribute("href");
     const section = document.querySelector(getHref);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+    section.scrollIntoView(this.option);
+  }
+
+  addLinkEvent() {
+    this.links.forEach((link) => {
+      link.addEventListener("click", this.scrollSuaveSections);
     });
   }
-  if (getInternalLinks.length) {
-    getInternalLinks.forEach((link) => {
-      link.addEventListener("click", scrollSuaveSections);
-    });
+
+  init() {
+    if (this.links.length) {
+      this.addLinkEvent();
+    }
+    return this;
   }
 }
