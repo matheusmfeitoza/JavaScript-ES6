9ac484a4787969ca0b84472744974e0bffb65b51
Matheus Marinho <feitozamatheus123@gmail.com>
2021-09-23 14:15:30 -0300

    Refactor animacao-Scroll

--- scripts/modulos/animacao-scroll.js
+++ scripts/modulos/animacao-scroll.js
@@ -1,20 +1,35 @@
-export default function animacaoAoScrollar() {
-  const sections = document.querySelectorAll("[data-scroll]");
-  function scrollEventSections() {
-    sections.forEach((item) => {
-      const windowMetade = window.innerHeight * 0.6;
-      const positionItemTop = item.getBoundingClientRect().top;
-      const isSectionVisible = positionItemTop - windowMetade < 0;
-      if (isSectionVisible) {
-        item.classList.add("ativo");
-      } else if (item.classList.contains("ativo")) {
-        item.classList.remove("ativo");
+export default class AnimacaoAoScrollar {
+  constructor(seletor) {
+    this.sections = document.querySelectorAll(seletor);
+    this.windowHeight = window.innerHeight * 0.6;
+    this.animarAoScrollar = this.animarAoScrollar.bind(this);
+  }
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const distancia = section.offsetTop;
+      return {
+        element: section,
+        distancia: Math.floor(distancia - this.windowHeight),
+      };
+    });
+  }
+
+  animarAoScrollar() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.distancia) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
-  if (sections.length) {
-    scrollEventSections();
 
-    window.addEventListener("scroll", scrollEventSections);
+  init() {
+    this.getDistance();
+    this.animarAoScrollar();
+    // this.scrollEventSections();
+    window.addEventListener("scroll", this.animarAoScrollar);
+    return this;
   }
 }
