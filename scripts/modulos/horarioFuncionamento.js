8dc93ff92a2c72f9a117dd0a9b32550a29a5f296
Matheus Marinho <feitozamatheus123@gmail.com>
2021-09-27 22:29:36 -0300

    New Refactor on horario funcionamento

--- scripts/modulos/horarioFuncionamento.js
+++ scripts/modulos/horarioFuncionamento.js
@@ -16,21 +16,39 @@ export default class HorarioFuncionamento {
   horaFuncionamento() {
     this.semana = this.semana.split(",").map(Number);
     this.horario = this.horario.split(",").map(Number);
+  }
 
-    const dataAgora = new Date();
-    const diaSemana =
-      this.semana.indexOf(dataAgora.getDay()) >= 0 &&
-      this.semana.indexOf(dataAgora.getDay()) <= 5;
-    const horarioSemana =
-      dataAgora.getHours() > this.horario[0] &&
-      dataAgora.getHours() <= this.horario[1];
-    if (diaSemana && horarioSemana) {
+  horaAgora() {
+    this.dataAgora = new Date();
+    this.diaSemanaAgora = this.dataAgora.getDay();
+    this.horaAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  verificaFuncionamento() {
+    const semanaAberto = this.semana.indexOf(this.diaSemanaAgora) !== -1;
+    const horarioAberto =
+      this.horaAgora >= this.horario[0] && this.horaAgora < this.horario[1];
+    return semanaAberto && horarioAberto;
+  }
+
+  // const dataAgora = new Date();
+  // const diaSemana = this.semana.indexOf(dataAgora.getDay()) !== -1;
+  // const horarioSemana =
+  //   dataAgora.getHours() > this.horario[0] &&
+  //   dataAgora.getHours() <= this.horario[1];
+
+  ativaFuncionamento() {
+    if (this.verificaFuncionamento()) {
       this.getHora.classList.add("aberto");
     }
   }
 
   init() {
-    this.horaFuncionamento();
+    if (this.getHora) {
+      this.horaFuncionamento();
+      this.horaAgora();
+      this.ativaFuncionamento();
+    }
     return this;
   }
 }
